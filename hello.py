14d686fd12a1210df54ccd98e3409a0665c0d467
lgf2111 <lgf2111@gmail.com>
2026-09-04 16:50:09 +0800

    Something fun to think about

--- hello.py
+++ hello.py
@@ -17,11 +17,14 @@ followers -= 10
 print("Day 3:", followers)
 
 username = input("Enter Username: ")
-age = input("Enter Age:")
-category = input("Enter Content Category:")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
 print("====================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what if fun for you??")
